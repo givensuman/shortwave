ab398c17a6acf7c80c0a186dbeb82fe866ccde41
given <givensuman@gmail.com>
2023-05-01 12:56:03 -0400

    Build landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,36 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Image, Heading, Center, Stack } from '@chakra-ui/react'
 
-import Search from "@/components/Search"
-import StationList from "@/components/StationLits"
-import { SearchProvider } from "@/hooks/useSearch"
+import Link from '@/components/Link'
+import logo from '../assets/logo.svg'
 
 export default function Home() {
   return (
-    <Box>
-      <SearchProvider>
-        <Search />
-        <StationList />
-      </SearchProvider>
+    <Box my="25vh">
+      <Center my="auto">
+        <Stack spacing={5}>
+          <Image 
+            src={logo.src}
+            alt="Shortwave Logo"
+            height={125}
+          />
+          <Heading size="lg">
+            Welcome to Shortwave
+          </Heading>
+          <Link 
+            href="/discover"
+            as={Center}
+          >
+            <Button
+              rounded="full"
+              colorScheme="blue"
+              width={60}
+              py={6}
+            >
+              Discover New Stations
+            </Button>
+          </Link>
+        </Stack>
+      </Center>
     </Box>
   )
 }
